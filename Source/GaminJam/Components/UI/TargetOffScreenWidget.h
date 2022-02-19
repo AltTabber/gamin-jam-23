27328b3eb34b2571f10476d3bd75c0e6eab229fa
AltTabber <mikeice414@gmail.com>
2022-02-19 18:26:12 +0300

    Начал делать карту, сделал заготовки под UI, если цель не видна камерой

---
+++ Source/GaminJam/Components/UI/TargetOffScreenWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TargetOffScreenWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMINJAM_API UTargetOffScreenWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	UFUNCTION(BlueprintCallable) FString AddGoalPointer();
+	
+};
